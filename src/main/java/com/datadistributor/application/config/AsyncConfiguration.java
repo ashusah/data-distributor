f11e0df3eff0f5f8498fe263e0989276c57ad4ca
ashu <ashusah@gmail.com>
2025-12-02 13:24:46 +0100

    Add test setup and refine async executor config

--- src/main/java/com/datadistributor/application/config/AsyncConfiguration.java
+++ src/main/java/com/datadistributor/application/config/AsyncConfiguration.java
@@ -4,6 +4,8 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -13,6 +15,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class AsyncConfiguration {
 
   @Bean(name = "dataDistributorTaskExecutor")
+  @Primary
   public ThreadPoolTaskExecutor dataDistributorTaskExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
     executor.setCorePoolSize(20);
@@ -25,7 +28,7 @@ public class AsyncConfiguration {
   }
 
   @Bean
-  public Executor rateLimitedExecutor(TaskExecutor dataDistributorTaskExecutor) {
+  public Executor rateLimitedExecutor(@Qualifier("dataDistributorTaskExecutor") TaskExecutor dataDistributorTaskExecutor) {
     return dataDistributorTaskExecutor;
   }
 }
